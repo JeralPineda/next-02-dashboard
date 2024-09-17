4ff02442dcdcef4459cca29249b3c115a7b61c37
Jeral Pineda <jeral.cris@gmail.com>
2024-09-16 23:03:37 -0600

    fix: ending section 02
    ending section 02

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,3 +1,17 @@
+import { CartCounter } from "@/app/shopping-cart";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "shoping Cart",
+  description: "Un simple contador",
+};
+
 export default function CounterPage() {
-  return <div>CounterPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+
+      <CartCounter />
+    </div>
+  );
 }
