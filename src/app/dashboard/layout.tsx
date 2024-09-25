c55b572c096821be330137d87c8b3acf8782a9d8
Jeral Pineda <jeral.cris@gmail.com>
2024-09-24 20:40:30 -0600

    fix: terminando la pagina del pokemon
    pokemon page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,17 @@
 import { Sidebar } from "@/components/sidebar";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
       <div className="flex">
         {/* sidebar  */}
         <Sidebar />
 
-        <div className="w-full  text-slate-900">{children}</div>
+        <div className="w-full  text-slate-900 p-4">{children}</div>
       </div>
     </div>
   );
