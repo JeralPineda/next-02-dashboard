5b1605491127953100ddafb75a02418811702f70
jcpmbhc <jpineda@banhcafe.hn>
2024-09-27 14:11:33 -0600

    fix: resolviendo tarea de la pagina pokemons/[name]
    agregando pagina de pokemon por nombre

--- src/pokemons/components/pokemon-card.tsx
+++ src/pokemons/components/pokemon-card.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 export const PokemonCard = ({ pokemon }: Props) => {
-  const { id, name } = pokemon;
+  const { name } = pokemon;
 
   return (
     <div className="mx-auto right-0 mt-2 w-60">
@@ -24,10 +24,12 @@ export const PokemonCard = ({ pokemon }: Props) => {
             priority={false}
           />
 
-          <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
+          <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">
+            {name}
+          </p>
           <div className="mt-5">
             <Link
-              href={`/dashboard/pokemon/${id}`}
+              href={`/dashboard/pokemon/${name}`}
               className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
             >
               Más información
@@ -35,12 +37,17 @@ export const PokemonCard = ({ pokemon }: Props) => {
           </div>
         </div>
         <div className="border-b">
-          <Link href="/dashboard/main" className="px-4 py-2 hover:bg-gray-100 flex items-center">
+          <Link
+            href="/dashboard/main"
+            className="px-4 py-2 hover:bg-gray-100 flex items-center"
+          >
             <div className="text-red-600">
               <IoHeartOutline />
             </div>
             <div className="pl-3">
-              <p className="text-sm font-medium text-gray-800 leading-none">No es favorito</p>
+              <p className="text-sm font-medium text-gray-800 leading-none">
+                No es favorito
+              </p>
               <p className="text-xs text-gray-500">View your campaigns</p>
             </div>
           </Link>
